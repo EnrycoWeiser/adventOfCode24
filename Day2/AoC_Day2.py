1a75f094b48d5164e1db147303a7172bc049ca5a
EnrycoWeiser <enricoferrarimb@gmail.com>
2024-12-02 16:05:37 +0100

    Day2_Part1

---
+++ Day2/AoC_Day2.py
@@ -0,0 +1,54 @@
+def check_order(a) -> bool:
+
+    order_asc = True
+    order_desc = True
+
+    # check ascending
+    for i in range(len(a) - 1):
+        if int(a[i]) >= int(a[i+1]):
+            order_asc = False
+            break
+
+    # check descending
+    if not order_asc:
+        for j in range(len(a) - 1):
+            if int(a[j]) <= int(a[j+1]):
+                order_desc = False
+                break
+
+    if order_asc:
+        return check_asc_step(a)
+    if order_desc:
+        return check_desc_step(a)
+    else:
+        return False    
+
+def check_asc_step(line) -> bool:
+    for z in range(len(line) - 1):
+        if int(line[z]) + 3 < (int(line[z+1])):
+            return False
+    return True
+
+def check_desc_step(line) -> bool:
+    for x in range(len(line) - 1):
+        if int(line[x]) - 3 > (int(line[x+1])):
+            return False
+    return True
+
+#     #             #       #
+####### END METHODS #########
+#     #             #       #
+
+with open('Day2\\input_Day2.txt') as file:
+    input = file.read().strip()
+    lines = input.split('\n')
+
+count = 0
+
+for l in lines:
+    line = l.split(' ')
+    if check_order(line):
+        count = count + 1
+    
+
+print(count)
